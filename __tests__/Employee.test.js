c5c86fe9fe7a8ca41cb373a5d41477eb263ac697
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-02-23 20:32:15 +0000

    Imports and exports

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,4 @@
+const Employee = require('../lib/Employee.js');
+
+// ES6 coversion 
+// import employee from './lib/Employee.js';
---
+++ index.js
@@ -0,0 +1,3 @@
+// External packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
