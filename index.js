f2af290ae7796f60c77e849ff54510176e5b1d8d
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-02-25 21:06:25 +0000

    File writing

--- index.js
+++ index.js
@@ -46,16 +46,74 @@ addEmployee = () => {
                     choices: ["yes", "no"],
                 }
             ])
-                .then((response2) => {
-                    if (response2.moreMembers == 'yes') {
+                .then((response) => {
+                    if (response.moreMembers == 'yes') {
                         addEmployee();
                     } else {
-                        console.log('Ready for HTML!');
+                        const html = generateHTML(response);
+                        fs.writeFile('team.html', html, err =>
+                            err ? console.log(err) : console.log('Success! Your HTML file has been generated!')
+                        );
+
+                        // console.log('Ready for HTML!');
                     }
                 })
         })
 };
 
+generateHTML = (response) => {
+    let htmlMain = `<!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  </head>
+  
+  <body>
+      <nav class="navbar navbar-light bg-info">
+          <span class="navbar-brand mx-auto">
+              <h1 class="text-white">Team Profile</h1>
+          </span>
+      </nav>
+      <br>
+      <br>
+      <section>
+          <div class="row d-flex justify-content-center">`
+    let htmlCARDS = addCards();
+
+    htmlMain += htmlCARDS;
+    
+    let htmlBOTTOM = `</div>
+  </section>
+</body>
+</html>`
+    htmlMain += htmlBOTTOM;
+
+    return htmlMain;
+}
+
+addCards = () => {
+    return `<div class="col-md-3">
+    <div class="card bg-primary mx-4">
+        <div class="card-body">
+            <div class="card-title text-white">
+                <h3>person 1</h3>
+                <h5>Manager</h5>
+            </div>
+            <ul class="list-group">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+            </ul>
+        </div>
+    </div>
+</div>`
+}
 
 init = () => {
     addEmployee()
