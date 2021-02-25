ead9e26c00e68284d4bea2eb91bd27dcbdce188f
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-02-25 13:13:41 +0000

    Manager questions added

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 // External packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+// Array of questions run by Inquirer
+const questions = [
+    {
+        type: 'list',
+        name: 'employeeType',
+        message: 'What type of employee would you like to add?',
+        choices: ['Manager','Engineer', 'Intern']
+    }, {
+        when: input => {
+            return input.employeeType == 'Manager'
+        },
+        type: 'input',
+        name: 'managerName',
+        message: 'What is the name of the manager?'
+    }, {
+        when: input => {
+            return input.employeeType == 'Manager'
+        },
+        type: 'input',
+        name: 'managerID',
+        message: 'What is the ID number of the manager?'
+    }, {
+        when: input => {
+            return input.employeeType == 'Manager'
+        },
+        type: 'input',
+        name: 'managerEmail',
+        message: 'What is the email of the manager?'
+    }, {
+        when: input => {
+            return input.employeeType == 'Manager'
+        },
+        type: 'input',
+        name: 'managerOffice',
+        message: 'What is the office number of the manager?'
+    },
+];
+
+function init() {
+    inquirer.prompt(questions)
+};
+
+init()
