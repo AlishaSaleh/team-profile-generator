f8e1ee1fa14f48bff3646f08183073df4e647e7c
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-02-25 17:29:59 +0000

    All questions running

--- index.js
+++ index.js
@@ -2,46 +2,63 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// Array of questions run by Inquirer
-const questions = [
-    {
-        type: 'list',
-        name: 'employeeType',
-        message: 'What type of employee would you like to add?',
-        choices: ['Manager','Engineer', 'Intern']
-    }, {
-        when: input => {
-            return input.employeeType == 'Manager'
+// Function for questions to add employees
+addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the employee?'
+        }, {
+            type: 'input',
+            name: 'id',
+            message: 'What is the ID number of the employee?'
+        }, {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email of the employee?'
         },
-        type: 'input',
-        name: 'managerName',
-        message: 'What is the name of the manager?'
-    }, {
-        when: input => {
-            return input.employeeType == 'Manager'
-        },
-        type: 'input',
-        name: 'managerID',
-        message: 'What is the ID number of the manager?'
-    }, {
-        when: input => {
-            return input.employeeType == 'Manager'
-        },
-        type: 'input',
-        name: 'managerEmail',
-        message: 'What is the email of the manager?'
-    }, {
-        when: input => {
-            return input.employeeType == 'Manager'
-        },
-        type: 'input',
-        name: 'managerOffice',
-        message: 'What is the office number of the manager?'
-    },
-];
+        {
+            type: 'list',
+            name: 'employeeRole',
+            message: 'What is the role of the employee?',
+            choices: ['Manager', 'Engineer', 'Intern']
+        }
+    ])
+        .then((response) => {
+            let roleSpec;
+            if (response.employeeRole == 'Manager') {
+                roleSpec = 'office number';
+            } else if (response.employeeRole == 'Engineer') {
+                roleSpec = 'GitHub username';
+            } else {
+                roleSpec = 'school name';
+            }
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'roleSpec',
+                    message: `What is the team member's ${roleSpec}?`
+                }, {
+                    type: "list",
+                    name: "moreMembers",
+                    message: "Would you like to add more team members?",
+                    choices: ["yes", "no"],
+                }
+            ])
+                .then((response2) => {
+                    if (response2.moreMembers == 'yes') {
+                        addEmployee();
+                    } else {
+                        console.log('Ready for HTML!');
+                    }
+                })
+        })
+};
 
-function init() {
-    inquirer.prompt(questions)
+
+init = () => {
+    addEmployee()
 };
 
 init()
