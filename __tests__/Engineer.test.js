9da243e43b31e627f3c630b375fabbeec94c9915
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-02-28 14:51:34 +0000

    Engineer test passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require('../lib/Engineer.js');
+
+test('Can get GitHub account via constructor', () => {
+    const testVal = 'GitHub';
+    const e = new Engineer('example', 1, 'example@test.com', testVal);
+    expect(e.github).toBe(testVal);
+});
+
+test('getRole should return \'Engineer\'', () => {
+    const testVal = 'Engineer';
+    const e = new Engineer('example', 1, 'example@test.com', 'GitHub');
+    expect(e.getRole()).toBe(testVal);
+});
+
+test('Get GitHub username via getGithub', () => {
+    const testVal = 'GitHub';
+    const e = new Engineer('example', 1, 'example@test.com', testVal);
+    expect(e.getGithub()).toBe(testVal);
+});
