c470ada3b27a94afbe19e875d0ede5ec2acc6828
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-02-28 15:26:30 +0000

    All tests passed

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
 const Intern = require('../lib/Intern.js');
+
+test('Can get school via constructor', () => {
+    const testVal = 'UoB';
+    const e = new Intern('example', 1, 'example@test.com', testVal);
+    expect(e.school).toBe(testVal);
+});
+
+test('getRole should return \'Intern\'', () => {
+    const testVal = 'Intern';
+    const e = new Intern('example', 1, 'example@test.com', 'UoB');
+    expect(e.getRole()).toBe(testVal);
+});
+
+test('Get school via getSchool', () => {
+    const testVal = 'UoB';
+    const e = new Intern('example', 1, 'example@test.com', testVal);
+    expect(e.getSchool()).toBe(testVal);
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,19 @@
 const Manager = require('../lib/Manager.js');
+
+test('Can get get office number via constructor', () => {
+    const testVal = 50;
+    const e = new Manager('example', 1, 'example@test.com', testVal);
+    expect(e.officeNumber).toBe(testVal);
+});
+
+test('getRole should return \'Manager\'', () => {
+    const testVal = 'Manager';
+    const e = new Manager('example', 1, 'example@test.com', 50);
+    expect(e.getRole()).toBe(testVal);
+});
+
+test('Get office number via getOfficeNumber', () => {
+    const testVal = 50;
+    const e = new Manager('example', 1, 'example@test.com', testVal);
+    expect(e.getOfficeNumber()).toBe(testVal);
+});
